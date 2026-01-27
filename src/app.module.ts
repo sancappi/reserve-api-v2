48650897abcd5ff43a360f6e1b78510c0dceb66b
Sandrs <sempresan@outlook.com>
2026-01-27 16:31:43 -0300

    configura o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { DbModule } from "./db/db.module";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-})
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), DbModule
+]})
 export class AppModule {}
